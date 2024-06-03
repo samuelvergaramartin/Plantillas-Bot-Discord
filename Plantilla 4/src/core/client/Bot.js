64a80747dc9c49e28c6919f034a3580a7a9d121b
samuelvergaramartin <accesopuerta0@gmail.com>
2024-06-03 21:50:13 +0200

    Created Bot.js

---
+++ Plantilla 4/src/core/client/Bot.js
@@ -0,0 +1,56 @@
+const { Client } = require('discord.js');
+
+class Bot extends Client {
+    botName;
+    botToken;
+    botCommands = [];
+    botEvents = [];
+    botStatus = null;
+    botPresenceMesaage = null;
+    botIntents = [];
+    botPartials = [];
+    botMentions = [];
+
+    static dataSchema = {
+        botName: undefined,
+        botToken: undefined,
+        botCommands: [],
+        botEvents: [],
+        botStatus: null,
+        botPresenceMesaage: null,
+        botIntents: [],
+        botPartials: [],
+        botMentions: []
+    };
+
+    constructor(data) {
+        if(data.botIntents) this.botIntents = data.botIntents;
+        if(data.botPartials) this.botPartials = data.botPartials;
+        if(data.botMentions) this.botMentions = data.botMentions;
+
+        super({
+            intents: this.botIntents,
+            partials: this.botPartials,
+            allowedMentions: {
+                parse: this.botMentions,
+                repliedUser: true
+            }
+        });
+
+        if(data.botName) this.botName = data.botName;
+        else this.botName = this.user.username;
+
+        if(!data.botToken) throw new Error("ERROR: The bot token was not provided");
+        else this.botToken = data.botToken;
+
+        if(data.botCommands) this.botCommands = data.botCommands;
+        
+        if(data.botEvents) this.botEvents = data.botEvents;ç
+
+        if(data.botStatus == null) this.botStatus = "online";
+        else this.botStatus = data.botStatus;
+
+        if(data.botPresenceMesaage == null) this.botPresenceMesaage = `${this.botName} : Ready`;
+        else this.botPresenceMesaage = data.botPresenceMesaage;
+    }
+}
