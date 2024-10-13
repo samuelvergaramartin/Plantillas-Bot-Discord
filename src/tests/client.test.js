bc2927717ff448425a506b2c69d4ff99bf1d29c7
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2024-10-13 11:37:23 +0200

    Añadido el test de verificacion de token del cliente.

--- src/tests/client.test.js
+++ src/tests/client.test.js
@@ -6,6 +6,15 @@ function checkIfIntentsAreLoaded() {
     else return false;
 }
 
+function checkIfTokenIsProvided() {
+    if(testBot.token && testBot.token != "" && testBot.token != null) return true;
+    else return false;
+}
+
 test('Carga de intents en el Cliente', () => {
-expect(checkIfIntentsAreLoaded()).toBe(true);
+    expect(checkIfIntentsAreLoaded()).toBe(true);
+});
+
+test('Token proporcionado en el cliente', () => {
+    expect(checkIfTokenIsProvided()).toBe(true);
 });
