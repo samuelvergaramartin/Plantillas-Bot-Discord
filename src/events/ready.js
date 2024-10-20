cb4be16b8d33e504239a9a1e5ba40651e2259787
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2024-10-20 14:22:40 +0200

    Ahora en el ready.js se muestra la data del bot en una tabla

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,15 @@
-const { green, blue } = require('colors');
+const { green, blue, magenta } = require('colors');
+const AsciiTable = require('ascii-table');
+
 module.exports = {
     name: "ready",
     once: false,
     activated: true,
     run: async(bot) => {
-        console.log(blue("Bot: " + bot.user.username));
-        console.log(green("Bot encendido y listo para la acción"));
+        const tableStatus = new AsciiTable(green("          All Systems Loaded!            "))
+        tableStatus.addRow(magenta('Bot'), blue(bot.user.username));
+        tableStatus.addRow(magenta('Status'), green("ONLINE"));
+
+        console.log(tableStatus.toString());
     }
 }
